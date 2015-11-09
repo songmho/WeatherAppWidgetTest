cf376d90bf79ed6eb85fa3198bc59573a7ebf8ca
songmho <songmho@gmail.com>
2015-11-09 16:56:38 +0900

    위젯생성

--- app/src/main/java/com/songmho/weatherappwidgettest/MainActivity.java
+++ app/src/main/java/com/songmho/weatherappwidgettest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.songmho.weatherappwidgettest;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -13,13 +15,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView title;
     ToggleButton toggleBt;
 
+    private boolean wf_onoff;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences settings= getSharedPreferences("weather", Context.MODE_PRIVATE);
+        wf_onoff=settings.getBoolean("onoff",false);
+        if(!wf_onoff){
+            Intent intent=new Intent(this,WeatherService.class);
+            stopService(intent);
+        }
+
         title=(TextView)findViewById(R.id.title);
         toggleBt=(ToggleButton)findViewById(R.id.toggleBt);
+        toggleBt.setChecked(wf_onoff);
+
         toggleBt.setOnClickListener(this);
     }
 
@@ -30,11 +43,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(!tb.isChecked()){
             Intent intent=new Intent(this,WeatherService.class);
             stopService(intent);
+            wf_onoff=false;
         }
         else{
             Intent intent=new Intent(this,WeatherService.class);
-          //  Log.d("dddd","ddddd");
             startService(intent);
+            wf_onoff=true;
         }
+
+        SharedPreferences settings=getSharedPreferences("weather",MODE_PRIVATE);
+        SharedPreferences.Editor editor=settings.edit();
+        editor.putBoolean("onoff",wf_onoff);
+        editor.commit();
     }
 }
--- app/src/main/java/com/songmho/weatherappwidgettest/WeatherAppWidgetProvider.java
+++ app/src/main/java/com/songmho/weatherappwidgettest/WeatherAppWidgetProvider.java
@@ -5,29 +5,43 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
 /**
  * Created by songmho on 2015-11-01.
  */
 public class WeatherAppWidgetProvider extends AppWidgetProvider {
+
+    private boolean wf_onoff;
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
+
+        SharedPreferences settings= context.getSharedPreferences("weather", Context.MODE_PRIVATE);
+        wf_onoff=settings.getBoolean("onoff",false);
+
         String region=intent.getStringExtra("region");
         String desc=intent.getStringExtra("desc");
         String ta=intent.getStringExtra("ta");
 
-        if(region==null)
-            region="unkown";
+        if(region==null || !wf_onoff) {
+            region = "unkown";
+            desc="";
+            ta="";
 
-        AppWidgetManager appWidgetManager=AppWidgetManager.getInstance(context);
+        }
 
-        RemoteViews remote=new RemoteViews(context.getPackageName(),R.layout.weatherview);
-        remote.setTextViewText(R.id.textView1, region);
-        remote.setTextViewText(R.id.textView2, desc);
-        remote.setTextViewText(R.id.textView3, ta);
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
 
-        appWidgetManager.updateAppWidget(new ComponentName("com.songmho.weatherappwidgettest","com.songmho.weatherappwidgettest.WeatherAppWidgetProvider"),remote);
+            RemoteViews remote = new RemoteViews(context.getPackageName(), R.layout.weatherview);
+            remote.setTextViewText(R.id.textView1, region);
+            remote.setTextViewText(R.id.textView2, desc);
+            remote.setTextViewText(R.id.textView3, ta);
+
+            appWidgetManager.updateAppWidget(new ComponentName("com.songmho.weatherappwidgettest", "com.songmho.weatherappwidgettest.WeatherAppWidgetProvider"), remote);
+
     }
+
+
 }
